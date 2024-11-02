10e0b2f0b93dc1f0dfe2fa722e230cdf15846448
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:16:48 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "arn" {
+  description = "ARN of the lambda function"
+  value       = one(aws_lambda_function.ssosync[*].arn)
+}
+
+output "invoke_arn" {
+  description = "Invoke ARN of the lambda function"
+  value       = one(aws_lambda_function.ssosync[*].invoke_arn)
+}
+
+output "qualified_arn" {
+  description = "ARN identifying your Lambda Function Version (if versioning is enabled via publish = true)"
+  value       = one(aws_lambda_function.ssosync[*].qualified_arn)
 }
